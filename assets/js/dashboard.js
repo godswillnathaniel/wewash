7a521dc8deaef42bfc35bbe78dc32747881bb165
techwithmazi <godswillnathaniel97@gmail.com>
2024-06-18 14:22:17 +0100

    working on the checkout orderpage

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -11,7 +11,7 @@ function updatePrice(priceId, selectElement, initialPrice) {
 // Populate select options
 const quantitySelects = document.querySelectorAll('.quantity')
 quantitySelects.forEach(select => {
-  for (let i = 0; i <= 20; i++) {
+  for (let i = 1; i <= 20; i++) {
     const option = document.createElement('option')
     option.value = i
     option.textContent = i
